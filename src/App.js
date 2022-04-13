b04b912d39c411e2b231edf34bbeea6a5eb0bcdf
HoracioGutierrez <horacio.estevez@gmail.com>
2022-04-12 21:07:49 -0300

    Primer desafio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+//import React from "react"
+//Fragmento
 
-function App() {
+//import React from "react"
+//import {Fragment} from "react"
+
+//<React.Fragment></React.Fragment>
+//<Fragment></Fragment>
+
+import Header from "./Header"
+import Footer from "./Footer"
+import Main from "./Main"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
 }
 
-export default App;
+export default App
+
+
+
+/* export default app = () => {
+  return "Hola Mundo"
+} */
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import Nav from "./Nav"
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; - 2022</p>
+      <p>Todos los derechos reservados</p>
+      <Nav/>
+    </footer>
+  )
+}
+
+export default Footer
